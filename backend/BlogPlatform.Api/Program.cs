ea4f8477007bc15c5ed98515863c52c7aecd4b7f
johnmiicheal <michealelijah301@gmail.com>
2025-09-28 20:03:21 +0100

    fix: i use postgres btw

--- backend/BlogPlatform.Api/Program.cs
+++ backend/BlogPlatform.Api/Program.cs
@@ -1,11 +1,47 @@
+using BlogPlatform.Api.Data;
+using BlogPlatform.Api.Models;
+using BlogPlatform.Api.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Configure PostgreSQL Database
+builder.Services.AddDbContext<BlogDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configure Identity
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+})
+.AddEntityFrameworkStores<BlogDbContext>()
+.AddDefaultTokenProviders();
+
+// Add application services
+builder.Services.AddScoped<IPostService, PostService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure CORS for development
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorApp", policy =>
+    {
+        policy.WithOrigins("https://localhost:5001", "http://localhost:5000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -18,6 +54,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Use CORS
+app.UseCors("AllowBlazorApp");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
